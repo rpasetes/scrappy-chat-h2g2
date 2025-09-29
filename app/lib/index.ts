cf2fb66c49200131a5131d14b4f44820b338ce3f
rpasetes <russap01@gmail.co>
2025-09-29 15:09:56 -0400

    better auth to drizzle migration

--- app/lib/index.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-// (1149) our db instance established here:
-const client = postgres(process.env.DATABASE_URL!)
-export const db = drizzle({ client });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/db/schema.ts",
+  schema: "./app/lib/auth-schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: { url: process.env.DATABASE_URL! },
