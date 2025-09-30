5a3e20a8925a3c8ab35ac07a5a517a2008fafea6
rpasetes <russap01@gmail.co>
2025-09-30 17:42:14 -0400

    hello anthropic

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
   route("api/ai", "routes/api.ai.ts"),
   route("api/auth/*", "routes/api.auth.$.ts"),
   route("protected", "routes/protected.tsx"),
+  route("chat", "routes/chat.tsx"),
 ] satisfies RouteConfig;
--- app/routes/api.ai.ts
+++ app/routes/api.ai.ts
@@ -2,8 +2,6 @@ import { anthropic } from '@ai-sdk/anthropic'
 import { streamText, convertToModelMessages, type UIMessage } from 'ai'
 import type { Route } from "./+types/api.ai";
 
-// (1635) ohh, adding custom route handler to 
-// routes.ts generates the appropriate types!
 export async function action({ request }: Route.ActionArgs) {
   const { messages }: { messages: UIMessage[] } = await request.json()
 
