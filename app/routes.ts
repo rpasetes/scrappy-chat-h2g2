8558bd0fb84dfd62fa31de0532f935447be812af
rpasetes <russap01@gmail.co>
2025-09-30 12:22:16 -0400

    making (and typing) a route from scratch

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("api/auth/*", "routes/api.auth.$.ts"),
+  route("protected", "routes/protected.tsx")
 ] satisfies RouteConfig;
