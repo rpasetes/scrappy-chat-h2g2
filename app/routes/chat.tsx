ea5b91a87adf17ca4f4cefcd6ca71ff5f3e742dc
rpasetes <russap01@gmail.co>
2025-10-01 11:34:55 -0400

    submit button style

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -2,46 +2,52 @@ import { useState } from "react"
 import { useChat } from "@ai-sdk/react"
 import { DefaultChatTransport } from "ai"
 import { Button } from "~/components/ui/button"
+import type { Route } from "./+types/chat"
 
-// (1707) personal nit: turned off readwise plugin
-// cause weird hydration errors in dev environment
-export default function Chat() {
+export default function Chat({ loaderData }: Route.ComponentProps) {
   const [input, setInput] = useState('')
   const { messages, sendMessage } = useChat({
     transport: new DefaultChatTransport({
-      api: 'api/ai' // config for route handler
+      api: 'api/ai'
     })
   })
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(message => (
-        <div key={message.id} className="whitespace-pre-wrap">
-          {message.role === 'user' ? 'User: ' : 'AI: '}
-          {message.parts.map((part, i) => {
-            switch (part.type) {
-              case 'text':
-                return <div key={`${message.id}-${i}`}>{part.text}</div>;
-            }
-          })}
-        </div>
-      ))}
+    <>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+        {messages.map(message => (
+          <div key={message.id} className="whitespace-pre-wrap">
+            {message.role === 'user' ? 'User: ' : 'AI: '}
+            {message.parts.map((part, i) => {
+              switch (part.type) {
+                case 'text':
+                  return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              }
+            })}
+          </div>
+        ))}
 
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          sendMessage({ text: input });
-          setInput('');
-        }}
-      >
-        <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={e => setInput(e.currentTarget.value)}
-        />
-      </form>
-      <Button>Click me</Button>
-    </div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            sendMessage({ text: input });
+            setInput('');
+          }}
+        >
+          <input
+            className="fixed dark:bg-stone-900 bottom-0 w-full max-w-md p-2 mb-11 border border-stone-300 dark:border-stone-800 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={e => setInput(e.currentTarget.value)}
+          />
+          <Button
+            className="fixed dark:bg-stone-800 hover:bg-stone-700 border border-stone-300 dark:border-stone-800 bottom-0"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </>
   );
 }
